b9c3dd7bd07f8bb3bf135f79f2c60660b3ee5f8d
Ricardo Moran <richi.moran@gmail.com>
2023-03-07 11:36:32 -0300

    Added markdown-to-hiccup dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [metasoarous/oz "2.0.0-alpha5" #_"1.6.0-alpha36"]]
+                 [metasoarous/oz "2.0.0-alpha5" #_"1.6.0-alpha36"]
+                 [markdown-to-hiccup "0.6.2"]]
   :main ^:skip-aot pbits-sreview.core
   :target-path "target/%s"
   :repl-options {:init-ns pbits-sreview.core
